ea27d6b87440231a2f761c81bdb72ef2489df1b5
LRiceBall <65839666+LRiceBall@users.noreply.github.com>
2020-08-30 16:21:43 -0500

    8_30_update

---
+++ rice_query.sql
@@ -0,0 +1,27 @@
+create table housing_df (
+	"State" varchar(30),
+    "City" varchar(20),
+	"January" dec,
+	"February" dec,
+	"March" dec,
+	"April" dec,
+	"May" dec,
+	"June" dec,
+	"July" dec,
+	"August" dec,
+	"September" dec,
+	"October" dec,
+	"November" dec,
+	"December" dec,
+	"AverageSales" dec,
+	"ZipCode" dec,
+	"Mean" dec,
+	"Median" dec,
+	"Stdev" dec
+	);
+
+select * from housing_df
+
+
+
+
