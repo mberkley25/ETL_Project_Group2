4439a70347b0e5be3c7aaf687be010738a68465d
LRiceBall <65839666+LRiceBall@users.noreply.github.com>
2020-09-01 20:02:00 -0500

    901 merge conflicts

--- schema.sql
+++ schema.sql
@@ -1,27 +1,39 @@
-create table housing_df (
-	"State" varchar(30),
-    "City" varchar(20),
-	"January" dec,
-	"February" dec,
-	"March" dec,
-	"April" dec,
-	"May" dec,
-	"June" dec,
-	"July" dec,
-	"August" dec,
-	"September" dec,
-	"October" dec,
-	"November" dec,
-	"December" dec,
-	"AverageSales" dec,
-	"ZipCode" dec,
-	"Mean" dec,
-	"Median" dec,
-	"Stdev" dec
+create table income (
+    id int,
+    "State" varchar(20),
+	"State_ab" varchar(2),
+	"County" varchar(30),
+	"City" varchar(30),
+	"Type" varchar(30),
+	"Primary" varchar(30),
+	"Zip_Code" varchar(30),
+	"Mean" int,
+	"Median" int,
+	"Stdev" int,
+	"sum_w" float,
+	PRIMARY KEY (State, City)
 	);
 
-select * from housing_df
+create table sales_2017 (
+	"City" varchar(30),
+	"State" varchar(30),
+	"January" float,
+	"February" float,
+	"March" float,
+	"April" float,
+	"May" float,
+	"June" float,
+	"July" float,
+	"August" float,
+	"September" float,
+	"October" float,
+	"November" float,
+	"December" float,
+	"Average_Sales" float,
+	Primary Key (State, City)
+);
 
+select * from sales_2017;
 
 
 
