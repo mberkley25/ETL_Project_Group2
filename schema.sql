ab5acee7de538ca51c9decf52b64da988b21d257
ElaineCheung <66335849+ElaineCheung@users.noreply.github.com>
2020-09-01 21:50:01 -0500

    Elaine
    updated city filter

---
+++ schema.sql
@@ -0,0 +1,42 @@
+create table income (
+    id int,
+    "State" varchar(20),
+	"State_ab" varchar(2),
+	"County" varchar(30),
+	"City" varchar(30),
+	"Type" varchar(30),
+	"Primary" varchar(30),
+	"Zip_Code" varchar(30),
+	"Mean" int,
+	"Median" int,
+	"Stdev" int,
+	"sum_w" float,
+	PRIMARY KEY ("State", "City")
+	);
+
+create table sales_2017 (
+	"City" varchar(30),
+	"State" varchar(30),
+	"January" float,
+	"February" float,
+	"March" float,
+	"April" float,
+	"May" float,
+	"June" float,
+	"July" float,
+	"August" float,
+	"September" float,
+	"October" float,
+	"November" float,
+	"December" float,
+	"AverageSales" float,
+	Primary Key ("State", "City")
+);
+
+select * from income;
+
+select * from sales_2017;
+
+
+
+
