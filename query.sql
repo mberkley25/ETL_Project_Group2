ba7903b7dcef6ea6d5229a386e020559d0e4c67a
Jason <stanghelle@gmail.com>
2020-09-01 20:56:11 -0500

    Added SQL Files

--- query.sql
+++ query.sql
@@ -1,3 +1,7 @@
 select * 
 from income 
 left join sales_2017 on income."City" = sales_2017."City" and income."State" = sales_2017."State";
+
+select *
+from income
+full outer join median_rental on income."StateAb" = median_rental."State" and income."County" = median_rental."County";
